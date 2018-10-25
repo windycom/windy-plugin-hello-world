a2b7455cb0300c174c13c4dfcf67bbfbce486adf
Ivo Lukacovic <ivo@windy.com>
2018-10-25 11:49:36 +0200

    v1.1.0

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,58 +1,48 @@
-W.loadPlugin(
+"use strict";
 
-  	/* Mounting options */
-  	{
-  "name": "windy-plugin-hello-world",
-  "version": "1.0.0",
+/**
+ * This is main plugin loading function
+ * Feel free to write your own compiler
+ */
+W.loadPlugin(
+/* Mounting options */
+{
+  "name": "windy-plugin-example",
+  "version": "1.1.0",
+  "author": "Windyty, S.E.",
   "repository": {
     "type": "git",
-    "url": "git+https://github.com/windycom/windy-plugin-hello-world.git"
+    "url": "git+https://github.com/windycom/windy-plugins"
   },
-  "author": "Windyty, S.E.",
-  "description": "The most simple, Windy plugin example.",
+  "description": "Windy plugin system enables anyone, with basic knowledge of Javascript to enhance Windy with new functionality (default desc).",
   "displayName": "Hello world",
   "hook": "menu"
 },
+/* HTML */
+'',
+/* CSS */
+'',
+/* Constructor */
+function () {
+  var map = W.require('map');
 
-  	/* HTML */
-	'',
+  console.log('I am being mounted');
+  var popup = null;
 
-	/* CSS */
-	'',
+  this.onopen = function () {
+    var center = map.getCenter();
 
-	/* Constructor */
-	function() {
+    if (popup) {
+      popup.setLatLng(center);
+    } else {
+      popup = L.popup().setLatLng(center).setContent("Hello World").openOn(map);
+    }
+  };
 
-	const map = W.require('map');
-
-
-	console.log('I am being mounted')
-
-	let popup = null
-
-	this.onopen = () => {
-
-		const center = map.getCenter()
-
-		if( popup ) {
-
-			popup.setLatLng(center)
-
-		} else {
-
-			popup = L.popup()
-            		.setLatLng(center)
-            		.setContent("Hello World")
-            		.openOn( map );
-		}
-	}
-
-	this.onclose = () => {
-		if( popup ) {
-			map.removeLayer( popup )
-			popup = null
-		}
-	}
-
-
+  this.onclose = function () {
+    if (popup) {
+      map.removeLayer(popup);
+      popup = null;
+    }
+  };
 });
